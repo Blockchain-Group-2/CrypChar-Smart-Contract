053cdbad0e1f59fa93f546e9310a74c18d932d01
GeorgeFane <39439818+GeorgeFane@users.noreply.github.com>
2020-08-16 00:53:06 -0400

    Update MetaCoin.sol

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.22 <0.7.0;
 contract MetaCoin {
 
     mapping(uint256 => uint256) public hash;
-    uint256 i=0;
+    uint256 public i=0;
 
     function store(uint256 num) public returns (bool success){
         hash[i]+=num;
@@ -11,7 +11,7 @@ contract MetaCoin {
         return true;
     }
     
-    function get(uint i) public view returns (uint256){
+    function get(uint256 i) public view returns (uint256){
         return hash[i];
     }
 }
